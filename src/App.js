ccfb1acc717d26a190c29dadb7df3ea240278f28
Albert <jaybhum@gmail.com>
2022-11-10 21:12:14 -0800

    Added graphql and storage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from "react";
-import { DataStore } from "@aws-amplify/datastore";
-import { Note } from "./models";
+// import { DataStore } from "@aws-amplify/datastore";
+// import { Note } from "./models";
 
 import "./App.css";
 import "@aws-amplify/ui-react/styles.css";
-import { API } from "aws-amplify";
+import { API, Storage } from "aws-amplify";
 import {
   Button,
   Flex,
   Heading,
+  Image,
   Text,
   TextField,
   View,
@@ -30,16 +31,28 @@ const App = ({ signOut }) => {
   async function fetchNotes() {
     const apiData = await API.graphql({ query: listNotes });
     const notesFromAPI = apiData.data.listNotes.items;
+    await Promise.all(
+      notesFromAPI.map(async (note) => {
+        if (note.image) {
+          const url = await Storage.get(note.name);
+          note.image = url;
+        }
+        return note;
+      })
+    )
     setNotes(notesFromAPI);
   }
 
   async function createNote(event) {
     event.preventDefault();
     const form = new FormData(event.target);
+    const image = form.get("image");
     const data = {
       name: form.get("name"),
       description: form.get("description"),
+      image: image.name,
     };
+    if (!!data.image) await Storage.put(data.name, image);
     await API.graphql({
       query: createNoteMutation,
       variables: { input: data },
@@ -51,6 +64,7 @@ const App = ({ signOut }) => {
   async function deleteNote({ id }) {
     const newNotes = notes.filter((note) => note.id !== id);
     setNotes(newNotes);
+    await Storage.remove(name);
     await API.graphql({
       query: deleteNoteMutation,
       variables: { input: { id } },
@@ -78,6 +92,12 @@ const App = ({ signOut }) => {
             variation="quiet"
             required
           />
+          <View 
+            name="image"
+            as="input"
+            type="file"
+            style={{ alignSelf: "end"}}
+          />
           <Button type="submit" variation="primary">
             Create Note
           </Button>
@@ -96,6 +116,13 @@ const App = ({ signOut }) => {
               {note.name}
             </Text>
             <Text as="span">{note.description}</Text>
+            {note.image && (
+              <Image
+                src={note.image}
+                alt={'visual aid for ${notes.name}'}
+                style={{ width: 400 }}
+              />
+            )}
             <Button variation="link" onClick={() => deleteNote(note)}>
               Delete note
             </Button>
@@ -108,11 +135,3 @@ const App = ({ signOut }) => {
 };
 
 export default withAuthenticator(App);
-
-await DataStore.save(
-  new Note({
-    "name": "Lorem ipsum..",
-    "description": "loreemmmm",
-    "image": "llllllloooorr"
-  })
-);
